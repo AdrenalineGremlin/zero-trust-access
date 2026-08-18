45da6640734e3514f96caabb56c42d44d9a92af4
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-18 16:43:08 -0400

    first commit

---
+++ src/main/java/zerotrust/zero_trust_access/ZeroTrustAccessApplication.java
@@ -0,0 +1,13 @@
+package zerotrust.zero_trust_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTrustAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroTrustAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zerotrust/zero_trust_access/ZeroTrustAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package zerotrust.zero_trust_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroTrustAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
