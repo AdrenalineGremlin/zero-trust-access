a4f3932b8d84d8ce748396f9edf57ccc474a21c3
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-18 21:34:30 -0400

    finished policyengine

--- src/main/java/zerotrust/zero_trust_access/security/PolicyEngine.java
+++ src/main/java/zerotrust/zero_trust_access/security/PolicyEngine.java
@@ -1,5 +1,27 @@
 package zerotrust.zero_trust_access.security;
 
-public class PolicyEngine {
+import zerotrust.zero_trust_access.model.RiskContext;
+import zerotrust.zero_trust_access.model.RiskLevel;
+import zerotrust.zero_trust_access.model.User;
 
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyEngine {
+    // compute trust score
+    public RiskLevel evalRisk(User user, RiskContext riskContext) {
+        boolean locationCheck = user.getKnownLocation() == null
+                || !user.getKnownLocation().equals(riskContext.getLocation());
+        boolean deviceCheck = user.getKnownDeviceId() == null
+                || !user.getKnownDeviceId().equals(riskContext.getDeviceID());
+        boolean lastVerifiedAt = user.getLastVerifiedAt() == null
+                || Duration.between(user.getLastVerifiedAt(), riskContext.getTimestamp()).toMinutes() > 30;
+        // unknown device, unknown location, too much time passed after veri
+        // = immediate medium risklevel
+        if (locationCheck || deviceCheck || lastVerifiedAt)
+            return RiskLevel.MEDIUM;
+        return RiskLevel.LOW;
+    }
 }
