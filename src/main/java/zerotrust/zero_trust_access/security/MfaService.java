f0fe8145fa0a7ac14f45a174041edc670eeef99d
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-18 22:22:16 -0400

    mfaserv finished

--- src/main/java/zerotrust/zero_trust_access/security/MfaService.java
+++ src/main/java/zerotrust/zero_trust_access/security/MfaService.java
@@ -1,5 +1,32 @@
 package zerotrust.zero_trust_access.security;
 
-public class MfaService {
+import org.springframework.stereotype.Service;
 
+import dev.samstevens.totp.code.CodeGenerator;
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+
+@Service
+public class MfaService {
+    // generate secret
+    public String generateSecret() {
+        SecretGenerator secretGenerator = new DefaultSecretGenerator();
+        return secretGenerator.generate();
+    }
+
+    // validate totp code
+    public boolean validateCode(String secret, String code) {
+        // TimeProvider because totp code are time-based
+        TimeProvider timeGen = new SystemTimeProvider();
+        // generates what the code would be now when secret is given
+        CodeGenerator codeGen = new DefaultCodeGenerator();
+        // compares code against what is should be valid now
+        CodeVerifier verifier = new DefaultCodeVerifier(codeGen, timeGen);
+        return verifier.isValidCode(secret, code);
+    }
 }
