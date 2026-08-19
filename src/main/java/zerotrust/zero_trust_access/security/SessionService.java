51bc95599b7e4cf5961cb8665ec028d433665d68
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-19 14:52:48 -0400

    finished service session

--- src/main/java/zerotrust/zero_trust_access/security/SessionService.java
+++ src/main/java/zerotrust/zero_trust_access/security/SessionService.java
@@ -1,5 +1,26 @@
 package zerotrust.zero_trust_access.security;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SessionService {
-    
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String createSession(String username) {
+        // generate session token
+        String generatedSessionToken = UUID.randomUUID().toString();
+        // store token as key and username as value for later lookup
+        redisTemplate.opsForValue().set(generatedSessionToken, username, 30, TimeUnit.MINUTES);
+        // give token back so it can be later returned to client
+        // for future requests
+        return generatedSessionToken;
+    }
+
+    public SessionService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 }
